0de5b3de83c64ecefd35d07e0f1d456c0eb75ef7
Douglas Miner <curr3nas@gmail.com>
2020-05-18 15:25:58 -0500

    dashboard functionality completed, needs styling

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -4,6 +4,11 @@ import TokenService from '../../services/token-service'
 
 class DashboardRoute extends Component {
 
+  state = {
+    language: {},
+    words: []
+  }
+
   componentDidMount() {
     let token = TokenService.getAuthToken()
     fetch(`${config.API_ENDPOINT}/language`, {
@@ -13,16 +18,31 @@ class DashboardRoute extends Component {
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data)
+        console.log(data.words)
+        this.setState({
+          language: data.language,
+          words: data.words,
+        })
       })
   }
+
   render() {
+    let language = this.state.language
+    let words = this.state.words
     return (
       <section>
-        <h2>Latin</h2>
-        <p>Total correct answers: language total score</p>
+        <h2>{language.name}</h2>
+    <p>Total correct answers: {language.total_score}</p>
         <a href='/learn'>Start practicing</a>
         <h3>Words to practice</h3>
+        {words.map(word => {
+          return (
+            <li>
+              <h4>{word.original}</h4>
+              <p>{`correct answer count: ${word.correct_count}`}</p> <p>{`incorrect answer count: ${word.incorrect_count}`}</p>
+            </li>
+          )
+        })}
       </section>
     );
   }
