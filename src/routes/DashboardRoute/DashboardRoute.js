0728a73aacb4437907b31ff712eff02dbf0d1ac9
Douglas Miner <curr3nas@gmail.com>
2020-05-18 15:02:30 -0500

    language GET fetch working

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import config from '../../config'
+import TokenService from '../../services/token-service'
 
 class DashboardRoute extends Component {
+
+  componentDidMount() {
+    let token = TokenService.getAuthToken()
+    fetch(`${config.API_ENDPOINT}/language`, {
+      headers: {
+        'Authorization': `bearer ${token}`
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  }
   render() {
     return (
       <section>
-        implement and style me
+        <h2>Latin</h2>
+        <p>Total correct answers: language total score</p>
+        <a href='/learn'>Start practicing</a>
+        <h3>Words to practice</h3>
       </section>
     );
   }
