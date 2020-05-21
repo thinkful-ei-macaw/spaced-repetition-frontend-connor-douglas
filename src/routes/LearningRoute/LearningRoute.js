dfa7c05ee6f0777d56fc3aa4670aedfefb40b290
Douglas Miner <curr3nas@gmail.com>
2020-05-21 12:05:40 -0500

    works but wont pass tests, still need to put int the correct view and next word buttons

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -4,16 +4,18 @@ import TokenService from '../../services/token-service'
 
 
 class LearningRoute extends Component {
-  
-  state = {
-    nextWord: '',
-    totalScore: 0,
-    wordCorrectCount: 0,
-    wordIncorrectCount: 0,
-    head: {},
-    submtted: false,
-    incorrect: false,
-    correct: true,
+  constructor() {
+    super() 
+    this.state = {
+      nextWord: '',
+      totalScore: 0,
+      wordCorrectCount: 0,
+      wordIncorrectCount: 0,
+      head: {},
+      submtted: false,
+      incorrect: false,
+      correct: true,
+    }
   }
 
 componentDidMount() {
@@ -25,7 +27,6 @@ componentDidMount() {
     })
     .then(res => res.json())
     .then(data => {
-      console.log(data.head)
       this.setState({
         nextWord: data.nextWord,
         totalScore: data.totalScore,
@@ -34,33 +35,37 @@ componentDidMount() {
         head: data.head
       })
     })
+    
   }
   
   answerSubmit(e) {
     e.preventDefault();
 
-    let reqBody = {guess: e.target['learn-guess-input'].value}
+    let reqBody = {"guess": e.target['learn-guess-input'].value}
     console.log(reqBody)
     let token = TokenService.getAuthToken()
+    console.log(token)
     fetch(`${config.API_ENDPOINT}/language/guess`, {
       method: 'POST',
       headers: {
-        'Authorization': `bearer ${token}`
+        'Authorization': `bearer ${token}`,
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(reqBody)
     })
     .then(res => res.json())
     .then(data => {
-      console.log('i did a thing')
-    })
-    this.setState({
+          this.setState({
       submitted: true,
       incorrect: true,
     })
+      console.log('i did a thing')
+    })
+
   }
 
   render() {
-    console.log(this.state.head)
+    console.log(this.state.head.translation)
     return (
       <section>
         {!this.state.submitted && <h2>Translate the word:</h2>}
@@ -76,7 +81,7 @@ componentDidMount() {
           <input id='learn-guess-input' type='text' required></input>
           <button type='submit'>Submit your answer</button>
         </form>}
-        {this.state.submitted && this.state.incorrect && <section className='DisplayFeedback'><p>The correct translation for {this.state.nextWord} was {this.state.head.value.answer}</p></section>}
+        {this.state.submitted && this.state.incorrect && <section className='DisplayFeedback'><p>The correct translation for {this.state.nextWord} was {this.state.head.translation}</p></section>}
         You have answered this word correctly {this.state.wordCorrectCount} times.
         You have answered this word incorrectly {this.state.wordIncorrectCount} times.
       </section>
